47209608acf7ff7a93ae0ee21ee65f9126b64c73
Anish George <anishgeorge2690@gmail.com>
2021-01-09 15:50:46 +0530

    feat: playground upgrade to vite 2.0 (#9)
    feat: bump up vue version
    
    fix: register change emit event

---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+const path = require('path')
+import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite'
+export default defineConfig({
+  plugins: [vue()],
+  root: 'demo',
+  build: {
+    outDir: 'demo_dist',
+  },
+  alias: {
+    '/@/': path.resolve(__dirname, 'src'),
+  },
+  optimizeDeps: {
+    include: ['sortablejs'],
+  },
+})
